4b2766fb10b7e95fda51bb487263241c7139375b
Nsk4u <nakulsahis6@gmail.com>
2025-12-18 21:38:54 +0530

    Delete assignment_01.py

--- assignment_01.py
+++
@@ -1,9 +0,0 @@
-print("welcome to python")
-a = int(input('enter 1 st no: '))
-b = int(input('enter 2 st no: '))
-add = a + b
-mul = a * b
-div = a/b
-print(add, mul, div)
-print("End of program")
-print("welcome to python")
